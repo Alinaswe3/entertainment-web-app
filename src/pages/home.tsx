648b1fe6fff5d0ddea15dfd08c1ab9b5cf80edc2
Alinaswe <alinaswe45@gmail.com>
2023-04-12 10:09:42 +0200

    Structured the main home page with the html and csss

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,18 +2,24 @@ import NavBar from "@/components/NavBar";
 import SearchBar from "@/components/SearchBar";
 
 const Home = () => {
-  return (
-    <main className="home">
-      <NavBar />
-      <div className="home__main">
-        <SearchBar />
-        <div className="movies">
-          <div className="movies__trending"></div>
-          <div className="movies__list"></div>
-        </div>
-      </div>
-    </main>
-  );
+    return (
+        <main className="home">
+            <NavBar/>
+            <div className="home__main">
+                <SearchBar/>
+                <div className="movies">
+                    <div className="movies__trending">
+                        <div className="trend-movies">
+
+                        </div>
+                    </div>
+                    <div className="movies__container">
+                        <div className="movies__list"></div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Home;

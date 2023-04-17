d111b4ce5b2d279982980452bd9e35ba99b4d5a5
Alinaswe <alinaswe45@gmail.com>
2023-04-17 15:01:38 +0200

    Made the landing page which routes to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
+import { Logo } from "@/components/Icons";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+/**
+ * Page component that renders the home page
+ * @component
+ */
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Pushing the user to login page after timer expires
+    const timer = setTimeout(() => {
+      router.push("/login");
+    }, 3000);
+    // Clearing the timer
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <>
-      <h1>Hello Next JS</h1>
-    </>
+    <div className="auth">
+      <div className="landing__content">
+        <h2 className="landing__heading">Welcome</h2>
+        <Logo />
+        <span className="loader"></span>
+      </div>
+    </div>
   );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 import FormInput from "@/components/FormInput";
 import Link from "next/link";
-import {Logo} from "@/components/Icons";
-import {useState} from "react";
+import { Logo } from "@/components/Icons";
+import { useState } from "react";
 
 /**
  * A log-in form component to handle user login
@@ -9,77 +9,64 @@ import {useState} from "react";
  * @returns {JSX.Element} The sign up form
  */
 const Login = () => {
+  // Setting the state for input values
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    // Setting the state for input values
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  /**
+   * Checks if input elements have values otherwise changes the CSS to show an error
+   * @returns void
+   */
+  const checkInput = () => {
+    if (email === "")
+      document.getElementById("email-error")?.classList.add("form-error");
+    else document.getElementById("email-error")?.classList.remove("form-error");
+    if (password === "")
+      document.getElementById("password-error")?.classList.add("form-error");
+    else
+      document.getElementById("password-error")?.classList.remove("form-error");
+  };
 
-    /**
-     * Checks if input elements have values otherwise changes the CSS to show an error
-     * @returns void
-     */
-    const checkInput = () => {
-        if (email === "")
-            document.getElementById("email-error")?.classList.add("form-error");
-        else
-            document.getElementById("email-error")?.classList.remove("form-error");
-        if (password === "")
-            document.getElementById("password-error")?.classList.add("form-error");
-        else
-            document
-                .getElementById("password-error")
-                ?.classList.remove("form-error");
-    };
+  /**
+   * handles the form submission of input data.
+   * @param event - the submit event of the HTML form
+   */
+  const handleLogin = (event) => {
+    event.preventDefault();
+    checkInput();
+  };
 
-    /**
-     * handles the form submission of input data.
-     * @param event - the submit event of the HTML form
-     */
-    const handleLogin = (event) => {
-        event.preventDefault();
-        checkInput();
-    };
-
-    return (
-        <main className="auth">
-            <Logo/>
-            <div className="auth__form">
-                <h1 className="mb-xmd heading--large">Login</h1>
-                <form className="form mb-smd" onSubmit={handleLogin}>
-                    <FormInput
-                        type={"email"}
-                        id={"email"}
-                        placeText={"Email address"}
-                        handleChange={setEmail}
-                    />
-                    <FormInput
-                        type={"password"}
-                        id={"password"}
-                        placeText={"Password"}
-                        handleChange={setPassword}
-                    />
-                    <button className="form__submit body-text body-text--medium">
-                        Login to your account
-                    </button>
-                </form>
-                <p className="auth--alt body-text body-text--medium">
-                    Don't have an account?&nbsp;
-                    <Link className="link" href="/signup">
-                        Sign up
-                    </Link>
-                </p>
-            </div>
-        </main>
-    );
+  return (
+    <main className="auth">
+      <Logo />
+      <div className="auth__form">
+        <h1 className="mb-xmd heading--large">Login</h1>
+        <form className="form mb-smd" onSubmit={handleLogin}>
+          <FormInput
+            type={"email"}
+            id={"email"}
+            placeText={"Email address"}
+            handleChange={setEmail}
+          />
+          <FormInput
+            type={"password"}
+            id={"password"}
+            placeText={"Password"}
+            handleChange={setPassword}
+          />
+          <button className="form__submit body-text body-text--medium">
+            Login to your account
+          </button>
+        </form>
+        <p className="auth--alt body-text body-text--medium">
+          Don't have an account?&nbsp;
+          <Link className="link" href="/signup">
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
-
-/*
-<div class="form-field">
-  <label for="name">Name:</label>
-  <input type="text" id="name" name="name" required>
-  <div class="error-message">Please enter your name.</div>
-</div>
-
- */
